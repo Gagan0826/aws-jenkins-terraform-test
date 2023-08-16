827217729b2a5a7f3e93a560e382a1319169b947
Gagan0826 <63014073+Gagan0826@users.noreply.github.com>
2023-08-16 18:33:50 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_instance" "" {
+resource "aws_instance" "demo" {
   ami           = "ami-0f5ee92e2d63afc18"
   instance_type = "t2.micro"
 
